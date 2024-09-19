d5f9808731e1c4ddcf0791d4e2b98b33446bcc05
anonymous-oracle <suhass149@outlook.com>
2024-09-19 21:56:31 +0530

    added a few models

---
+++ README.txt
@@ -0,0 +1,7 @@
+- django project init/create
+django-admin startproject core .
+
+- build a store by starting the app; store is the name of the app
+py manage.py startapp store
+
+- add the app (here it is store) name in core/settings.py under the apps list which should be around the middleware list
---
+++ store/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True) # a slug field is part of the request URI path which can act as a service/resource identifier; in this case it can be /<category_name>/.../<product_name>, any special characters entered will be formatted in a URL/URI safe manner
+
+    class Meta:
+        verbose_name_plural = 'categories' # django may use plural form of the Model name i.e., Category as Categorys which is wrong, so an additional data which is the plural alternative is provided
+    
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE) # if a category is deleted, then all products linked to that category or belonging to that category are deleted
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creator')
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255, default='admin')
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images/') # upload images to this folder
+    slug = models.SlugField(max_length=255)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True) # fills in the timestamp value when the entry is created
+    updated = models.DateTimeField(auto_now=True) # updates the field when the entry is updated
+
+    class Meta:
+        verbose_name_plural = 'Products'
+        ordering = ('-created') # when the data is returned it sorts by the created timestamp in desc order
+        # ordering = ('created') # when the data is returned it sorts by the created timestamp in asc order
+
+    def __str__(self) -> str:
+        return self.title
